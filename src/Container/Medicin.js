8c4093c061bd0d4dc27b02a378f5b76fe6317866
Abhaykotadiya <abhaykotadiya22@gmail.com>
2022-05-16 08:52:49 -0700

    ok

---
+++ src/Container/Medicin.js
@@ -0,0 +1,13 @@
+import  Dialog  from './Dialog';
+import React from 'react';
+
+function Medicin(props) {
+    return (
+        <div>
+            <h1>Medicin</h1>
+            <Dialog />
+        </div>
+    );
+}
+
+export default Medicin;
