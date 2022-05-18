8db1028c014df3b87caddf0bc304fd67431da68f
khushaldobariya <khushaldobariya.77@gmail.com>
2022-05-18 08:47:23 +0530

    SUBMIT

--- src/Container/Dialog.js
+++ src/Container/Dialog.js
@@ -1,19 +1,67 @@
-import * as React from 'react';
-import React, { useState } from 'react';
+
+import React, { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
+import { DataGrid } from '@mui/x-data-grid';
+
 
 
 export default function FormDialog() {
-  const [open, setOpen] = React.useState(false);
-  const [name, setName] = React.useState('');
-  const [price, setprice] = React.useState('');
-  const [quntity, setquntity] = React.useState('');
-  const [expiry, setexpiry] = React.useState('');
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState('');
+  const [price, setprice] = useState('');
+  const [quntity, setquntity] = useState('');
+  const [expiry, setexpiry] = useState('');
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+
+      headerName: 'First name',
+      width: 150,
+      editable: true,
+    },
+    {
+
+      headerName: 'Last name',
+      width: 150,
+      editable: true,
+    },
+    {
+
+      headerName: 'Age',
+      type: 'number',
+      width: 110,
+      editable: true,
+    },
+    {
+
+      headerName: 'Full name',
+      sortable: false,
+      width: 160,
+     
+    },
+  ];
+
+const laoddata = () => {
+  let laocaldata = JSON.parse(loacalStorge.getItem("medicine"))
+}
+
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  ];
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -32,18 +80,20 @@ export default function FormDialog() {
       quntity,
       expiry,
     }
+    console.log(data);
+    if (localdata === null) {
+      localStorage.setItem("Medicine", JSON.stringify([data]))
+    } else {
+      localdata.push(data)
+      localStorage.setItem("Medicine", JSON.stringify(localdata))
+    }
+  
   }
-  console.log(data);
-  if (localdata === null) {
-    localStorage.setItem("Medicine", JSON.stringify([loacaldata]))
-  } else {
-    localdata.push(data)
-    localStorage.setItem("Medicine", JSON.stringify(data))
-  }
-
+ 
 
 
   return (
+    
     <div>
       <Button variant="outlined" onClick={handleClickOpen}>
         Add Medicine
@@ -61,8 +111,9 @@ export default function FormDialog() {
             type="name"
             fullWidth
             variant="standard"
+            onChange={(e)=> setName (e.target.value)}
 
-          />
+          /> 
 
           <TextField
             autoFocus
@@ -73,6 +124,7 @@ export default function FormDialog() {
             type="Number"
             fullWidth
             variant="standard"
+            onChange={(e)=> setprice (e.target.value)}
           />
 
           <TextField
@@ -84,6 +136,7 @@ export default function FormDialog() {
             type="Number"
             fullWidth
             variant="standard"
+            onChange={(e)=> setquntity (e.target.value)}
           />
 
           <TextField
@@ -94,6 +147,8 @@ export default function FormDialog() {
             type="date"
             fullWidth
             variant="standard"
+            onChange={(e)=> setexpiry (e.target.value)}
+
           />
         </DialogContent>
         <DialogActions>
